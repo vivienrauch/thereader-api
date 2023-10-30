8ffe4b421af3d03d54aa29f24cc26856643df6d8
vivienrauch <vivienmirjam@gmail.com>
2023-10-30 22:13:21 +0000

    Add unit tests to PostDetail view to test functionality

--- posts/tests.py
+++ posts/tests.py
@@ -29,3 +29,31 @@ class PostListViewTests(APITestCase):
     def test_user_not_logged_in_cant_create_post(self):
         response = self.client.post('/posts/', {'title': 'test title'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class PostDetailViewTests(APITestCase):
+    def setUp(self):
+        testuser1 = User.objects.create_user(username='testuser1', password='test1')
+        testuser2 = User.objects.create_user(username='testuser2', password='test2')
+        Post.objects.create(
+            owner=testuser1, title='title1', content='testcontent 1'
+        )
+        Post.objects.create(
+            owner=testuser2, title='title2', content='testcontent 2'
+        )
+
+    def test_can_retrieve_post_using_valid_id(self):
+        response = self.client.get('/posts/1/')
+        self.assertEqual(response.data['title'], 'title1')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cant_retrieve_post_using_invalid_id(self):
+        response = self.client.get('posts/4/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_update_own_post(self):
+        self.client.login(username='testuser1', password='test1')
+        response = self.client.put('/posts/1/', {'title': 'new title'})
+        post = Post.objects.filter(pk=1).first()
+        self.assertEqual(post.title, 'new title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
