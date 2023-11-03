e5270d27f4e2e367d868d00a654d7aa66f34523a
vivienrauch <vivienmirjam@gmail.com>
2023-11-03 21:18:45 +0000

    Add Post and Profile filters using django filters

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from thereader_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
@@ -18,7 +19,8 @@ class PostList(generics.ListAPIView):
     ).order_by('-created_at')
     filter_backends = [
         filters.OrderingFilter,
-        filters.SearchFilter
+        filters.SearchFilter,
+        DjangoFilterBackend
     ]
     ordering_fields = [
         'likes_count',
@@ -28,6 +30,14 @@ class PostList(generics.ListAPIView):
     search_fields = [
         'owner__username',
         'title'
+    ]
+    filterset_fields =[
+        # user feed
+        'owner__followed__owner__profile',
+        # user liked posts
+        'likes__owner__profile',
+        # user posts
+        'owner__profile'
     ]
 
     def perform_create(self, serializer):
