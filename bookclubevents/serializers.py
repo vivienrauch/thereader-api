ee84c374e451f8dc350b34925cf092b5325ba323
vivienrauch <vivienmirjam@gmail.com>
2023-11-05 21:46:03 +0000

    Add part of event serializer methods

--- bookclubevents/serializers.py
+++ bookclubevents/serializers.py
@@ -10,3 +10,22 @@ class BookClubEventSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(sosurce='owner.profile.image.url')
+
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image size exceeds 2MB!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width exceeds 4096px!'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height exceeds 4096px!'
+            )
+        return value
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
